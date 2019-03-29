1cac0ec6a12a098cac62e5d1d9add80617620efb
Resse Christophe <christophe.resse@gmail.com>
2019-03-29 16:55:21 +0100

    feature(annotation): Added dedicated annotations.

---
+++ common/src/main/java/com/kakoo/foundation/common/annotation/Experimental.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright(c) 2018 Hemajoo Ltd.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Hemajoo's Incubation Software (HIS) project which is licensed
+ * under the Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ *
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package com.kakoo.foundation.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+/**
+ * Annotation used to declare a Java type as being experimental.<br>
+ * Be aware that, by using such Java types, Hemajoo can at any moment change the
+ * implementation of such classes.
+ * <hr>
+ * @author  <a href="mailto:christophe.resse@gmail.com">Resse Christophe - Hemajoo</a>
+ * @version 1.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Experimental
+{
+    // EMPTY
+}
---
+++ common/src/main/java/com/kakoo/foundation/common/annotation/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains test artifacts for the Kakoo's Foundation Common component dedicated to the annotations.
+ */
+package com.kakoo.foundation.common.annotation;
